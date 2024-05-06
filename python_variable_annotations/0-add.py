f9fd84ac0802f396343f95dfc45af8435307949f
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-05-06 15:46:11 -0500

    task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function add that takes a float a and a float b as arguments and returns their sum as a float.
+"""
+def add(a: float, b: float) -> float:
+    """ Args:
+        a (float): a float integer
+        b (float): b float integer
+
+    Returns:
+        the sum of a and b
+    """
+    return a + b
