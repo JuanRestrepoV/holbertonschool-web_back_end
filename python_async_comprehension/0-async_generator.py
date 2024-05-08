2f0d7691b0d4c283d5e0a106c1d11d2a34f2f7db
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-05-08 18:06:59 -0500

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,7 +11,7 @@ from random import uniform
 from typing import AsyncIterator, AsyncGenerator
 
 
-async def async_generator() -> AsyncIterator[ AsyncGenerator[int, None] ]:
+async def async_generator() -> AsyncIterator[ AsyncGenerator[float, None, None] ]:
     """
     No arguments
     """
