9360359b1bf7248954b74b9c69677904552b0ab6
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-05-08 17:10:34 -0500

    task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+coroutine will loop 10 times,
+Seach time asynchronously
+wait 1 second, then yield
+Sa random number between 0
+Sand 10. Use the random module.
+"""
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """
+    No arguments
+    """
+    for _ in range (0, 11):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
