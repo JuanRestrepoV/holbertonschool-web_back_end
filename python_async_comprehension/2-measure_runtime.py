a29bd612e16a170658bb86e303eaf9cddae48d49
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-05-09 20:46:51 -0500

    task 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+measure_runtime should
+measure the total runtime
+and return it.
+"""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> int:
+    """
+    measure_runtime should
+    measure the total runtime
+    and return it.
+    """
+    start_time = time.time()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    return time.time() - start_time
