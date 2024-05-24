2001f8bbe6d00ede3fd069f022cd2a63620e2c6c
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-05-23 21:47:24 -0500

    task 4

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,8 +1,7 @@
 export default function returnHowManyArguments(...Args) {
-  let numberOfParameters = 0
+  let numberOfParameters = 0;
   for (const arg of Args) {
     numberOfParameters += 1;
   }
   return numberOfParameters;
-  console.log(numberOfParameters)
 }
