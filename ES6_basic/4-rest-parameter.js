dd8bb6b0fb0ad1c7cccfa54e0850c2e25f05bf18
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-05-23 21:49:48 -0500

    task 4 eslint fixed

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,7 +1,10 @@
 export default function returnHowManyArguments(...Args) {
   let numberOfParameters = 0;
+  /* eslint-disable */
   for (const arg of Args) {
     numberOfParameters += 1;
   }
+  /* eslint-disable */
   return numberOfParameters;
+  console.log(numberOfParameters)
 }
