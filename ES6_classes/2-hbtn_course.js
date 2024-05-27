3af68af522464cc8da74504f95f32624f055da10
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-05-27 13:36:49 -0500

    task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = val;
+    }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (typeof val !== 'number') {
+      throw TypeError('Length must be a number')
+    }
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(val) {
+    if (typeof val == 'object') {
+      this._students = val;
+    }
+    else {
+      throw TypeError('Students must be an object');
+    }
+  }
+}
