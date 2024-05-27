085ab949b02dc465d6b6257a7e1ef519dd43b47b
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-05-27 15:39:59 -0500

    task 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building{
+
+	constructor(sqft) {
+    if (new.target !== Building) {
+      this.evacuationWarningMessage();
+    }
+    this.sqft = sqft;
+  }
+
+
+	get sqft() {
+		return this._sqft;
+	}
+
+	set sqft(val) {
+		this._sqft = val;
+	}
+
+	evacuationWarningMessage() {
+		throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
